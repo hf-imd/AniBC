585a69575881d0d60fb3a8f51669e694ad2b8344
Oliver Steiner <oliver@mollo.ch>
2017-11-23 22:40:42 +0100

    first init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, NgZone} from '@angular/core';
+import {PouchdbService} from './pouchdb.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,51 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
-}
+    public people: Array<any>;
+    public form: any;
+
+    public constructor(private database: PouchdbService, private zone: NgZone) {
+        this.people = [];
+        this.form = {
+            'username': '',
+            'firstname': '',
+            'lastname': ''
+        };
+
+      //  this.ngOnInit();
+    }
+
+    public ngOnInit() {
+        this.database.sync('http://localhost:5984/anibc');
+        this.database.getChangeListener().subscribe(data => {
+            for (let i = 0; i < data.change.docs.length; i++) {
+                this.zone.run(() => {
+                    this.people.push(data.change.docs[i]);
+                });
+            }
+        });
+        this.database.fetch().then(result => {
+            this.people = [];
+            for (let i = 0; i < result.rows.length; i++) {
+                this.people.push(result.rows[i].doc);
+            }
+        }, error => {
+            console.error(error);
+        });
+    }
+
+    public insert() {
+        if (this.form.username && this.form.firstname && this.form.lastname) {
+
+            console.log('insert ');
+
+            this.database.put(this.form.username, this.form);
+
+            // reset form inputs
+            this.form = {
+                'username': '',
+                'firstname': '',
+                'lastname': ''
+            };
+        }
+    }}
---
+++ src/app/pouchdb.service.ts
@@ -0,0 +1,95 @@
+import { Injectable, EventEmitter } from '@angular/core';
+import PouchDB from 'pouchdb';
+
+@Injectable()
+export class PouchdbService {
+
+    private isInstantiated: boolean;
+    private database: any;
+    private listener: EventEmitter<any> = new EventEmitter();
+
+    public constructor() {
+        if (!this.isInstantiated) {
+            this.database = new PouchDB('anibc');
+            console.log(this.database.adapter); // prints either 'idb' or 'websql'
+
+            this.isInstantiated = true;
+        }
+    }
+    /**
+     *
+     *
+     * @returns {any}
+     */
+    public fetch() {
+        return this.database.allDocs({include_docs: true});
+
+    }
+
+    /**
+     *
+     * @param {string} id
+     */
+    public get(id: string) {
+        return this.database.get(id);
+    }
+
+    /**
+     *
+     *
+     * @param {string} id
+     * @param document
+     */
+    public put(id: string, document: any) {
+
+        // build id from form.username
+        document._id = id;
+
+        // is there already some Docomunt with this id in the DB ?
+        return this.get(id).then(result => {
+            document._rev = result._rev;
+            return this.database.put(document);
+        }, error => {
+
+            // if not, put it in
+            if (error.status == '404') {
+                
+                return this.database.put(document).then(function (response) {
+                    // handle response
+                }).catch(function (err) {
+                    console.log(err);
+                });
+
+            } else {
+
+                return new Promise((resolve, reject) => {
+                    reject(error);
+                });
+            }
+        });
+    }
+
+    /**
+     *
+     *
+     * @param {string} remote
+     */
+    public sync(remote: string) {
+        const remoteDatabase = new PouchDB(remote);
+        this.database.sync(remoteDatabase, {
+            live: true
+        }).on('change', change => {
+            this.listener.emit(change);
+        });
+    }
+
+    /**
+     *
+     *
+     * @returns {EventEmitter<any>}
+     */
+    public getChangeListener() {
+        return this.listener;
+    }
+}
+
