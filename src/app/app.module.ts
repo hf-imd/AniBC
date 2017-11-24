6d0aff34bf088629ba8d511d29b18a18faea9c5d
Oliver Steiner <oliver@mollo.ch>
2017-11-24 17:17:22 +0100

    add Admin Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,19 @@ import {FormsModule} from '@angular/forms';
 
 import {AppComponent} from './app.component';
 import {PouchdbService} from './pouchdb.service';
-import { AlphabetListComponent } from './alphabet-list/alphabet-list.component';
-import { AuthorsListComponent } from './authors-list/authors-list.component';
+import {AlphabetListComponent} from './alphabet-list/alphabet-list.component';
+import {AuthorsListComponent} from './authors-list/authors-list.component';
+import { UploadFormComponent } from './upload-form/upload-form.component';
+import { AdminBoardComponent } from './admin-board/admin-board.component';
 
 
 @NgModule({
     declarations: [
         AppComponent,
-        CharlistComponent,
         AlphabetListComponent,
-        AuthorsListComponent
+        AuthorsListComponent,
+        UploadFormComponent,
+        AdminBoardComponent
     ],
     imports: [
         BrowserModule,
