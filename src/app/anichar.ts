a3705bde9fc928c3f39e5db4bb3532a98b247977
Oliver Steiner <oliver@mollo.ch>
2017-12-20 01:04:55 +0100

    upload form ok

---
+++ src/app/anichar.ts
@@ -0,0 +1,14 @@
+
+export class Anichar {
+    constructor(public id: string,
+                public anichar?: string,
+                public first_name?: string,
+                public last_name?: string,
+                public organisation?: string,
+                public version?: string,
+                public sound?: boolean,
+                public long?: boolean,
+                public filename_movie?: string, // .mp4
+                public filename_doku?: string,) {
+    }
+}
